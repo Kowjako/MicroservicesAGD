7c801ff89d8ca2884f4cf9202195eb5b9cd788d4
Wlodzimierz <kowyako@yandex.ru>
2023-04-09 23:02:35 +0200

    Completed Catalog MongoDb Context & options pattern

---
+++ MicroservicesSrc/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Extensions;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(IOptions<MongoSettings> config)
+        {
+            var client = new MongoClient(config.Value.ConnectionString);
+            var db = client.GetDatabase(config.Value.DatabaseName);
+
+            Products = db.GetCollection<Product>(config.Value.CollectionName);
+            CatalogContextSeed.SeedData(Products);
+        }
+
+    }
+}
---
+++ MicroservicesSrc/Services/Catalog/Catalog.API/Extensions/MongoSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Extensions
+{
+    public class MongoSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
