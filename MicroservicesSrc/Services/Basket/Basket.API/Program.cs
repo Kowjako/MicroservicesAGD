b8257b4552c073d8af936c43b04a9655eb3403b8
Wlodzimierz <kowyako@yandex.ru>
2023-04-10 21:34:35 +0200

    Reworked Redis cache to use IConnectionMultiplexer

--- MicroservicesSrc/Services/Basket/Basket.API/Program.cs
+++ MicroservicesSrc/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,4 @@
+using Basket.API.Repositories;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,7 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(opt =>
     var options = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"));
     return ConnectionMultiplexer.Connect(options);
 });
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
--- MicroservicesSrc/Services/Basket/Basket.API/Repositories/BasketRepository.cs
+++ MicroservicesSrc/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -1,27 +1,27 @@
 ﻿using Basket.API.Entities;
-using Microsoft.Extensions.Caching.Distributed;
 using Newtonsoft.Json;
+using StackExchange.Redis;
 
 namespace Basket.API.Repositories
 {
     public class BasketRepository : IBasketRepository
     {
-        private readonly IDistributedCache _redisCache;
+        private readonly IDatabase _redisCache;
 
-        public BasketRepository(IDistributedCache redisCache) => _redisCache = redisCache;
+        public BasketRepository(IConnectionMultiplexer mlpx) => _redisCache = mlpx.GetDatabase();
 
         public async Task DeleteBasket(string username)
-            => await _redisCache.RemoveAsync(username);
+            => await _redisCache.KeyDeleteAsync(username);
         
         public async Task<ShoppingCart> GetBasket(string username)
         {
-            var basket = await _redisCache.GetStringAsync(username);
-            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+            var basket = await _redisCache.StringGetAsync(username);
+            return basket.IsNullOrEmpty ? null : JsonConvert.DeserializeObject<ShoppingCart>(basket);
         }
 
         public async Task<ShoppingCart> UpdateBasket(ShoppingCart cart)
         {
-            await _redisCache.SetStringAsync(cart.Username, JsonConvert.SerializeObject(cart));
+            await _redisCache.StringSetAsync(cart.Username, JsonConvert.SerializeObject(cart));
             return await GetBasket(cart.Username);
         }
     }
