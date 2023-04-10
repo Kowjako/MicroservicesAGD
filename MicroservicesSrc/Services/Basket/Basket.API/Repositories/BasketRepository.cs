116660fbf2c97c2411ca5f145b1c90afbb5c43ef
Wlodzimierz <kowyako@yandex.ru>
2023-04-10 17:27:42 +0200

    Implemented basket repository

---
+++ MicroservicesSrc/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,28 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache) => _redisCache = redisCache;
+
+        public async Task DeleteBasket(string username)
+            => await _redisCache.RemoveAsync(username);
+        
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket = await _redisCache.GetStringAsync(username);
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart cart)
+        {
+            await _redisCache.SetStringAsync(cart.Username, JsonConvert.SerializeObject(cart));
+            return await GetBasket(cart.Username);
+        }
+    }
+}
