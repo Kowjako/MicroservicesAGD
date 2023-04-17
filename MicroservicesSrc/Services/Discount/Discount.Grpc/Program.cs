7960e84fea408f3c070944bd05439bf89b7bae1d
Wlodzimierz <kowyako@yandex.ru>
2023-04-17 15:41:50 +0200

    Add AutoMapper for grpc service & prepare first test method to get discount

--- MicroservicesSrc/Services/Discount/Discount.Grpc/Program.cs
+++ MicroservicesSrc/Services/Discount/Discount.Grpc/Program.cs
@@ -1,14 +1,17 @@
 using Discount.Grpc.Extensions;
 using Discount.Grpc.Repository;
+using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 await app.MigratePgDatabase();
 
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ MicroservicesSrc/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repository;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repo;
+        private readonly ILogger<DiscountService> _logger;
+        private readonly IMapper _mapper;
+
+        public DiscountService(IDiscountRepository repo, ILogger<DiscountService> logger, IMapper mapper)
+            => (_repo, _logger, _mapper) = (repo, logger, mapper);
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request,
+            ServerCallContext context)
+        {
+
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request,
+            ServerCallContext context)
+        {
+
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _repo.GetDiscount(request.ProductName);
+            if (coupon is null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, "Can't find specific discount"));
+            }
+            _logger.LogInformation($"Discount was retrieved for productName = {request.ProductName}");
+            return _mapper.Map<CouponModel>(coupon);
+        }
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request,
+            ServerCallContext context)
+        {
+
+        }
+    }
+}
