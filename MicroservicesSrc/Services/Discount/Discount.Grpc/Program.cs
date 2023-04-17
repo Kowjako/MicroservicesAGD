049dc43a91b802a17c5cc6702c95dc3e1b7050c9
Wlodzimierz <kowyako@yandex.ru>
2023-04-17 15:24:46 +0200

    Prepare discount protobuf grpc service file

--- MicroservicesSrc/Services/Discount/Discount.Grpc/Program.cs
+++ MicroservicesSrc/Services/Discount/Discount.Grpc/Program.cs
@@ -1,6 +1,5 @@
 using Discount.Grpc.Extensions;
 using Discount.Grpc.Repository;
-using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +9,6 @@ builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 var app = builder.Build();
 await app.MigratePgDatabase();
 
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
