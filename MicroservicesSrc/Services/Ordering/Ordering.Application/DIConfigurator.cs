10cb3cffbafd200868acf119f52fbbced6298ea7
Wlodzimierz <kowyako@yandex.ru>
2023-04-19 17:39:58 +0200

    Added services registration for application layer

---
+++ MicroservicesSrc/Services/Ordering/Ordering.Application/DIConfigurator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System.Reflection;
+
+namespace Ordering.Application
+{
+    public static class DIConfigurator
+    {
+        public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(x => x.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptioBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
