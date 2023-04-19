776f4f711a08db64ab783983a9909d12957a5e51
Wlodzimierz <kowyako@yandex.ru>
2023-04-19 17:27:56 +0200

    Prepare application behaviors using MediatR pipelines

---
+++ MicroservicesSrc/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptioBehavior.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviours
+{
+    // Pipeline from MediatR - of course we can use default exception middleware/filter for that
+    public class UnhandledExceptioBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptioBehavior(ILogger<TRequest> logger)
+            => _logger = logger;
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, $"Application error occured for {requestName}");
+                throw;
+            }
+        }
+    }
+}
---
+++ MicroservicesSrc/Services/Ordering/Ordering.Application/Behaviours/ValidationBehavior.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MediatR;
+using CE = Ordering.Application.Exceptions;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        // collection of validators will be injected from DI
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        // MediatR pipleine = default middleware
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var results = await Task.WhenAll(_validators.Select(p => p.ValidateAsync(context, cancellationToken)));
+                var failures = results.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                    throw new CE.ValidationException(failures);
+            }
+            return await next();
+        }
+    }
+}
