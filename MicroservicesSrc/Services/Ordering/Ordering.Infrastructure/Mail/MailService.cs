58aeabbe09e33e9e6e9cc22690f141d2724e5a78
Wlodzimierz <kowyako@yandex.ru>
2023-04-19 18:33:25 +0200

    Implemented MailService using SendGrid &  Order repository

---
+++ MicroservicesSrc/Services/Ordering/Ordering.Infrastructure/Mail/MailService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
+
+namespace Ordering.Infrastructure.Mail
+{
+    public class MailService : IEmailService
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public MailService(IOptions<EmailSettings> emailSettings) => _emailSettings = emailSettings.Value;
+
+        public async Task<bool> SendEmailAsync(Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+            var msg = MailHelper.CreateSingleEmail(new EmailAddress(_emailSettings.FromAddress, _emailSettings.FromName),
+                                                   new EmailAddress(email.To),
+                                                   email.Subject,
+                                                   email.Body,
+                                                   email.Body);
+
+            var response = await client.SendEmailAsync(msg);
+
+            return response.StatusCode switch
+            {
+                var x when x == HttpStatusCode.Accepted || x == HttpStatusCode.OK => true,
+                _ => false
+            };
+        }
+    }
+}
---
+++ MicroservicesSrc/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext context) : base(context) { }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            return await GetAsync(x => x.UserName.Equals(userName));
+        }
+    }
+}
