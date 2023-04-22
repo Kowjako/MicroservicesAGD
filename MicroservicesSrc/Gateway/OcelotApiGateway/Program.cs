73b8c66af2966c081709bc865e5556617ad10b61
Wlodzimierz <kowyako@yandex.ru>
2023-04-22 18:07:33 +0200

    Added ocelot caching to catalog endpoint

--- MicroservicesSrc/Gateway/OcelotApiGateway/Program.cs
+++ MicroservicesSrc/Gateway/OcelotApiGateway/Program.cs
@@ -1,5 +1,6 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,8 @@ builder.Logging.AddDebug();
 /* Load custom json to IConfiguration */
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot()
+                .AddCacheManager(s => s.WithDictionaryHandle());
 
 var app = builder.Build();
 
